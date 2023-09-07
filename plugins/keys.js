424b21ff2b7afbece5602e0de5fa47ff113f9557
Pablo <trmaayt@gmail.com>
2023-09-07 17:42:34 +0200

    better keys

--- plugins/keys.js
+++ plugins/keys.js
@@ -10,118 +10,27 @@ let dArrow = false;
 let lArrow = false;
 let rArrow = false;
 
-let mouseX = 0;
-let mouseY = 0;
-let mouseXinit = 0;
-let mouseYinit = 0;
-let mousemove = false;
-
 window.addEventListener('keydown', function(e){
-    if(e.keyCode == "87"){ 
-        wDown = true;
-    }
-    if(e.keyCode == "83"){ 
-        sDown = true;
-    } 
-    if(e.keyCode == "65"){ 
-        aDown = true;
-    } 
-    if(e.keyCode == "68"){ 
-        dDown = true;
-    } 
-    if(e.keyCode == "81"){ 
-        qDown = true;
-    } 
-    if(e.keyCode == "69"){ 
-        eDown = true;
-    }
-    if(e.keyCode == "38"){
-        uArrow = true;
-    }
-    if(e.keyCode == "40"){
-        dArrow = true;
-    }
-    if(e.keyCode == "39"){
-        rArrow = true;
-    }
-    if(e.keyCode == "37"){
-        lArrow = true;
-    }
+    wDown = e.keyCode == "87"?true:wDown;
+    sDown = e.keyCode == "83"?true:sDown;
+    aDown = e.keyCode == "65"?true:aDown;
+    dDown = e.keyCode == "68"?true:dDown;
+    qDown = e.keyCode == "81"?true:qDown;
+    eDown = e.keyCode == "69"?true:eDown;
+    uArrow = e.keyCode == "38"?true:uArrow;
+    dArrow = e.keyCode == "40"?true:dArrow;
+    rArrow = e.keyCode == "39"?true:rArrow;
+    lArrow = e.keyCode == "37"?true:lArrow;
 });
 window.addEventListener('keyup', function(e){
-    if(e.keyCode == "87"){ 
-        wDown = false;
-    }
-    if(e.keyCode == "83"){ 
-        sDown = false;
-    } 
-    if(e.keyCode == "65"){ 
-        aDown = false;
-    } 
-    if(e.keyCode == "68"){ 
-        dDown = false;
-    } 
-    if(e.keyCode == "81"){ 
-        qDown = false;
-    } 
-    if(e.keyCode == "69"){ 
-        eDown = false;
-    }
-    if(e.keyCode == "38"){
-        uArrow = false;
-    }
-    if(e.keyCode == "40"){
-        dArrow = false;
-    }
-    if(e.keyCode == "39"){
-        rArrow = false;
-    }
-    if(e.keyCode == "37"){
-        lArrow = false;
-    }
+    wDown = e.keyCode == "87"?false:wDown;
+    sDown = e.keyCode == "83"?false:sDown;
+    aDown = e.keyCode == "65"?false:aDown;
+    dDown = e.keyCode == "68"?false:dDown;
+    qDown = e.keyCode == "81"?false:qDown;
+    eDown = e.keyCode == "69"?false:eDown;
+    uArrow = e.keyCode == "38"?false:uArrow;
+    dArrow = e.keyCode == "40"?false:dArrow;
+    rArrow = e.keyCode == "39"?false:rArrow;
+    lArrow = e.keyCode == "37"?false:lArrow;
 });
-
-document.addEventListener('mousemove', function(event) {
-    mouseX = event.pageX;
-    mouseY = event.pageY;
-    mousemove = true;
-});
-
-// let cln = [0,0,0,0,0,0];
-// setInterval(function(){
-//     if(wDown){
-//         cln[0]++;
-//         if(cln[0] >= 100){
-//             wDown = false;
-//             cln[0] = 0;
-//         }
-//     }
-//     if(sDown){
-//         cln[1]++;
-//         if(cln[1] >= 100){
-//             sDown = false;
-//             cln[1] = 0;
-//         }        
-//     }
-//     if(dDown){
-//         cln[2]++;
-//         if(cln[2] >= 100){
-//             dDown = false;
-//             cln[2] = 0;
-//         }
-//     }
-//     if(aDown){
-//         cln[3]++;
-//         if(cln[3] >= 100){
-//             aDown = false;
-//             cln[3] = 0;
-//         }
-//     }
-//     if(rArrow){
-//         cln[4]++;
-//         if(cln[4] >= 100){
-//             rArrow = false;
-//             cln[4] = 0;
-//         }
-//     }
-// },1);
